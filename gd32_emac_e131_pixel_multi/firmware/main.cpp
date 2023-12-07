eea113a0478d4ecc73cdd3182437882c889cbb5e
Arjan van Vught <arjan.van.vught@gmail.com>
2023-12-07 17:17:02 +0100

    Sync with local development

--- gd32_emac_e131_pixel_multi/firmware/main.cpp
+++ gd32_emac_e131_pixel_multi/firmware/main.cpp
@@ -100,9 +100,9 @@ void main() {
 	display.TextStatus(NetworkConst::MSG_MDNS_CONFIG, Display7SegmentMessage::INFO_MDNS_CONFIG, CONSOLE_YELLOW);
 
 	MDNS mDns;
-	mDns.AddServiceRecord(nullptr, mdns::Services::CONFIG, "node=sACN E1.31 Pixel");
+	mDns.ServiceRecordAdd(nullptr, mdns::Services::CONFIG, "node=sACN E1.31 Pixel");
 #if defined (ENABLE_HTTPD)
-	mDns.AddServiceRecord(nullptr, mdns::Services::HTTP);
+	mDns.ServiceRecordAdd(nullptr, mdns::Services::HTTP);
 #endif
 	mDns.Print();
 
--- gd32_emac_pp_pixel_multi/firmware/main.cpp
+++ gd32_emac_pp_pixel_multi/firmware/main.cpp
@@ -97,10 +97,10 @@ void main() {
 	display.TextStatus(NetworkConst::MSG_MDNS_CONFIG, Display7SegmentMessage::INFO_MDNS_CONFIG, CONSOLE_YELLOW);
 
 	MDNS mDns;
-	mDns.AddServiceRecord(nullptr, mdns::Services::CONFIG, "node=PixelPusher");
-	mDns.AddServiceRecord(nullptr, mdns::Services::PP);
+	mDns.ServiceRecordAdd(nullptr, mdns::Services::CONFIG, "node=PixelPusher");
+	mDns.ServiceRecordAdd(nullptr, mdns::Services::PP);
 #if defined (ENABLE_HTTPD)
-	mDns.AddServiceRecord(nullptr, mdns::Services::HTTP);
+	mDns.ServiceRecordAdd(nullptr, mdns::Services::HTTP);
 #endif
 	mDns.Print();
 
